2ed6d74dfb69ea30b2c97a5b8f140f7d7e9710a5
SakamotoHiroya <ayorih16@icloud.com>
2025-03-29 18:15:40 +0900

    wip

--- discord_agent.py
+++ discord_agent.py
@@ -9,11 +9,11 @@ async def create_discord_agent(client: discord.Client, guild_id: int):
     agent = Agent(
         name="discord seacher",
         instructions="ユーザの指示に忠実に従い、discord内を検索したり、メッセージの要約を取得してください。",
-        # tools=[
-        #     await create_fetch_messages_tool(client),
-        #     await create_list_text_channels_tool(client, guild_id),
-        #     await create_list_threads_tool(client)
-        # ]
+        tools=[
+            await create_fetch_messages_tool(client),
+            await create_list_text_channels_tool(client, guild_id),
+            await create_list_threads_tool(client)
+        ]
     )
     
     return agent
