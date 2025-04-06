8ae380673d0a4d95993f4c625289ae2bbe5562f4
Tsubasa-2005 <a.t.s.y.i.2314@icloud.com>
2025-04-06 14:25:36 +0900

    fix init_notion_client test

--- src/discora/agents/notion/clinet_test.py
+++ src/discora/agents/notion/clinet_test.py
@@ -1,21 +1,21 @@
 import os
 import unittest
-from notion_client import Client
-from src.discora.service.notion.client import init_notion_client
+from notion_client import AsyncClient
+from discora.service.notion.client import init_notion_client
 
-class TestInitNotionClient(unittest.TestCase):
+class TestInitNotionClient(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.token = os.getenv("NOTION_TOKEN")
         if not self.token:
             raise ValueError("NOTION_TOKEN environment variable is not set.")
-        
-    def test_client_with_token(self):
-        client = init_notion_client(self.token)
-        self.assertIsInstance(client, Client)
 
-    def test_client_invalid_token(self):
-        client = init_notion_client("invalid_token")
-        self.assertIsInstance(client, Client)
+    async def test_client_with_token(self):
+        client = await init_notion_client(self.token)
+        self.assertIsInstance(client, AsyncClient)
+
+    async def test_client_invalid_token(self):
+        client = await init_notion_client("invalid_token")
+        self.assertIsInstance(client, AsyncClient)
 
 if __name__ == "__main__":
     unittest.main()
